a2f2c16c8e678c5d1a30f36d0794722f00222917
janakievska.vesna <janakievska.vesna@gmail.com>
2021-11-06 14:30:49 +0100

    without decimal check

--- Calculator/Form1.Designer.cs
+++ Calculator/Form1.Designer.cs
@@ -61,6 +61,7 @@ namespace Calculator
             this.button1.TabIndex = 0;
             this.button1.Text = "0";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Click);
             // 
             // button2
             // 
@@ -71,6 +72,7 @@ namespace Calculator
             this.button2.TabIndex = 1;
             this.button2.Text = "1";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_Click);
             // 
             // button3
             // 
@@ -81,6 +83,7 @@ namespace Calculator
             this.button3.TabIndex = 2;
             this.button3.Text = "2";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_Click);
             // 
             // button4
             // 
@@ -91,6 +94,7 @@ namespace Calculator
             this.button4.TabIndex = 3;
             this.button4.Text = "3";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_Click);
             // 
             // button5
             // 
@@ -101,6 +105,7 @@ namespace Calculator
             this.button5.TabIndex = 4;
             this.button5.Text = "4";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_Click);
             // 
             // button6
             // 
@@ -111,6 +116,7 @@ namespace Calculator
             this.button6.TabIndex = 5;
             this.button6.Text = "5";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_Click);
             // 
             // button7
             // 
@@ -121,6 +127,7 @@ namespace Calculator
             this.button7.TabIndex = 6;
             this.button7.Text = "6";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_Click);
             // 
             // button8
             // 
@@ -131,6 +138,7 @@ namespace Calculator
             this.button8.TabIndex = 7;
             this.button8.Text = "7";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_Click);
             // 
             // button9
             // 
@@ -141,6 +149,7 @@ namespace Calculator
             this.button9.TabIndex = 8;
             this.button9.Text = "8";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button_Click);
             // 
             // button10
             // 
@@ -151,6 +160,7 @@ namespace Calculator
             this.button10.TabIndex = 9;
             this.button10.Text = "9";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button_Click);
             // 
             // button11
             // 
@@ -161,6 +171,7 @@ namespace Calculator
             this.button11.TabIndex = 10;
             this.button11.Text = ",";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button_Click);
             // 
             // button12
             // 
@@ -171,6 +182,7 @@ namespace Calculator
             this.button12.TabIndex = 11;
             this.button12.Text = "+";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.operator_Click);
             // 
             // button13
             // 
@@ -181,6 +193,7 @@ namespace Calculator
             this.button13.TabIndex = 12;
             this.button13.Text = "-";
             this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.operator_Click);
             // 
             // button14
             // 
@@ -191,6 +204,7 @@ namespace Calculator
             this.button14.TabIndex = 13;
             this.button14.Text = "*";
             this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.operator_Click);
             // 
             // button15
             // 
@@ -201,6 +215,7 @@ namespace Calculator
             this.button15.TabIndex = 14;
             this.button15.Text = "/";
             this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.operator_Click);
             // 
             // button16
             // 
@@ -211,6 +226,7 @@ namespace Calculator
             this.button16.TabIndex = 15;
             this.button16.Text = "=";
             this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.ednakvo_Click);
             // 
             // button17
             // 
--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -12,10 +12,84 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        int brOperacii = 0;
+        Decimal vrednost = 0;
+        String operacija = "";
+        bool operacija_klik = false;
         public Form1()
         {
             InitializeComponent();
         }
 
+        //this is for all btns with character number
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (operacija_klik || txt_result.Text == "0")
+                txt_result.Clear();
+
+            Button b = (Button)sender;
+            txt_result.Text += b.Text;
+
+            operacija_klik = false;
+        }
+
+        private void operator_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            brOperacii++;
+
+            if (brOperacii > 1)
+            {
+                switch (operacija)
+                {
+                    case "+":
+                        vrednost += Decimal.Parse(txt_result.Text);
+                        break;
+                    case "-":
+                        vrednost -= Decimal.Parse(txt_result.Text);
+                        break;
+                    case "*":
+                        vrednost *= Decimal.Parse(txt_result.Text);
+                        break;
+                    case "/":
+                        vrednost /= Decimal.Parse(txt_result.Text);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+                vrednost = Decimal.Parse(txt_result.Text);
+
+            operacija = b.Text;
+            lbl_izraz.Text = vrednost + " " + operacija;
+            operacija_klik = true;
+
+        }
+
+        private void ednakvo_Click(object sender, EventArgs e)
+        {
+            lbl_izraz.Text = "";
+            switch (operacija)
+            {
+                case "+":
+                    txt_result.Text = (vrednost + Decimal.Parse(txt_result.Text)).ToString();
+                    break;
+                case "-":
+                    txt_result.Text = (vrednost - Decimal.Parse(txt_result.Text)).ToString();
+                    break;
+                case "*":
+                    txt_result.Text = (vrednost * Decimal.Parse(txt_result.Text)).ToString();
+                    break;
+                case "/":
+                    txt_result.Text = (vrednost / Decimal.Parse(txt_result.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+            vrednost = 0;
+            brOperacii = 0;
+            operacija_klik = true;
+        }
     }
 }
